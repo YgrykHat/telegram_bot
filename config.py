358fe61b618d63a2ca08cc9971aeb1618aeb0faa
igor <40393850+YgrykHat@users.noreply.github.com>
2019-03-24 10:42:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 token = '842244382:AAFSN1LVE0yWXxINPV7WS6qej4q_jm-ENMs'
-chat = 'id' # here you need to enter the chat ID in which the bot will write.
+key = 'hCSwftePMbpnX4c2jQwwgGWGML8Bx3nDZmsWSj4W76XGDtwz4B'
+start_mess = 'start message'
+help_mess = 'I can not help you'
