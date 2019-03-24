d16e102c4002a49612a88fd02f0c0c78b1adb4c9
igor <40393850+YgrykHat@users.noreply.github.com>
2019-03-24 10:42:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,15 +8,14 @@ import re
 import os.path
 import subprocess
 
-start_mess = 'start message'
-help_mess = 'I can not help you'
-
 bot = telebot.TeleBot(config.token)
+
+#chat = '' # here you need to enter the chat ID in which the bot will write.
 
 zi = os.path.exists('img.zip')
 
 if zi == True:
-	subprocess.Popen(["python", "unziper.py"])
+	subprocess.Popen(['python', 'unziper.py'])
 else:
 	pass
 
@@ -38,29 +37,39 @@ def get_url_dog():
     url = contents['url']
     return url
 
+@bot.message_handler(commands = [config.key])
+def id(message):
+	f = open('id.txt', 'w')
+	f.write(str(message.chat.id))
+	f.close()
+	print(message.chat.id)
+
+di = os.path.exists('id.txt')
+if di == True:
+	f = open('id.txt')		
+	chat = f.read()
+	f.close()
+else:
+	pass
+	
 @bot.message_handler(commands = ['start'])
 def start(message):
-	bot.send_message(message.chat.id, start_mess)
+	bot.send_message(message.chat.id, config.start_mess)
 	# print(message.chat.id)
 
 @bot.message_handler(commands = ['help'])
 def help(message):
-	bot.send_message(message.chat.id, help_mess)
+	bot.send_message(message.chat.id, config.help_mess)
 
 @bot.message_handler(commands = ['dog'])
 def dog(message):
     url = get_url_dog()
-    chat_id = message.chat.id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    chat.id = message.chat.id
+    bot.send_photo(message.chat.id, photo = url)
 
 @bot.message_handler(content_types = ['text'])
 def repeat_all_messages(message):
-	text = ''
-	for x in message.text:
-		if True:
-			text += x
-	print(text)
-	# bot.send_message(config.chat, text) # if you want the bot to send all messages a certain chat.
+	 bot.send_message(chat, message.text) # if you want the bot to send all messages a certain chat.
 
 def main():
     updater = Updater(bot)
