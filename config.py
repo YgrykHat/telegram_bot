13e78bc09e7287d8b960db4c4b6cce0d940516f6
igor <40393850+YgrykHat@users.noreply.github.com>
2020-09-14 00:03:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-token = '842244382:AAFSN1LVE0yWXxINPV7WS6qej4q_jm-ENMs'
-key = 'hCSwftePMbpnX4c2jQwwgGWGML8Bx3nDZmsWSj4W76XGDtwz4B'
+token = ''
+key = ''
 start_mess = 'start message'
 help_mess = 'I can not help you'
-chat = '538024314'
+chat = 'chat id'
